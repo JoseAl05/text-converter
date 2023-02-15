3f1f0a7b20e74b8f549fe54e37781097fbf34afd
JoseAl05 <jparancibialinker@gmail.com>
2023-02-15 19:09:32 -0300

    increase max tokens

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -13,7 +13,7 @@ async function translateText(req: NextApiRequest, res: NextApiResponse) {
             model:'text-davinci-003',
             prompt:`Translate this into ${req.query.language} : ${req.query.text}`,
             temperature:0.3,
-            max_tokens: 100,
+            max_tokens: 1000,
         })
         res.status(200).json(translate.data)
     } catch (error) {
